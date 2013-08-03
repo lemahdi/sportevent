c85e42e7bdf83f91e358cd69905b821a1257493f
Mohammed Mahdi Akkouh <akkouh.mahdi@gmail.com>
2013-08-03 20:09:10 +0200

    starting the sport event web site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sportevent::Application.config.secret_key_base = '65239c055d52c61ce5e3d889a000b0dd7ae1a85ff7e0069fac7597dc3880d523614dd54496f963416ad9e2dcd6e8a414609234bba3cddb426c1af61f4b25dc9d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sportevent::Application.config.session_store :encrypted_cookie_store, key: '_sportevent_session'
