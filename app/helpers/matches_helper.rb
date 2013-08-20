72b73adb0eaf2537b515c79c2d4e111f8d6b77c6
Mohammed Mahdi Akkouh <akkouh.mahdi@gmail.com>
2013-08-20 23:13:22 +0200

    refactoring match views

--- app/helpers/matches_helper.rb
+++ app/helpers/matches_helper.rb
@@ -4,4 +4,8 @@ module MatchesHelper
 		fields = Field.all
 		names = fields.map { |field| [field.name, field.id] }.select { |f| f }
 	end
+
+	def match_end(match)
+		(match.start + match.duration.minutes).strftime('%H:%M')
+	end
 end
