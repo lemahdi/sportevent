e03d91cbf7d963dcab18c0cdd4c1e8ef69584aff
Mohammed Mahdi Akkouh <akkouh.mahdi@gmail.com>
2013-08-29 08:40:40 +0200

    changing migration name

--- db/migrate/20130829063612_add_utc_offset_to_fields.rb
+++ db/migrate/20130829063612_add_utc_offset_to_fields.rb
@@ -1,9 +1,9 @@
-class AddZoneToFields < ActiveRecord::Migration
+class AddUtcOffsetToFields < ActiveRecord::Migration
   def up
     add_column :fields, :utc_offset, :string, default: "+00:00"
   end
 
   def down
-  	remove_column :fields, :zone
+  	remove_column :fields, :utc_offset
   end
 end
