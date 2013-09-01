abe46e619f64c30b24e637e9045a8bd585229971
Mohammed Mahdi Akkouh <akkouh.mahdi@gmail.com>
2013-09-01 20:12:12 +0200

    removing useless migrations

--- db/migrate/20130829063612_add_utc_offset_to_fields.rb
+++
@@ -1,9 +0,0 @@
-class AddUtcOffsetToFields < ActiveRecord::Migration
-  def up
-    add_column :fields, :utc_offset, :string, default: "+00:00"
-  end
-
-  def down
-  	remove_column :fields, :utc_offset
-  end
-end
