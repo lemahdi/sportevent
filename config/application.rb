3356ff4109714fe9293d1fdc97cf9ae43252c8c5
Mohammed Mahdi Akkouh <akkouh.mahdi@gmail.com>
2013-11-02 20:14:34 +0100

    environment variables are managed by a local file

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,15 @@ module Sportevent
     config.i18n.default_locale = :fr
 
     config.assets.initialize_on_precompile = false
+
+    if Rails.env.development?
+      config.before_configuration do
+        env_file = File.join(Rails.root, 'config', 'local_env.yml')
+        YAML.load(File.open(env_file)).each do |key, value|
+          ENV[key.to_s] = value
+        end if File.exists?(env_file)
+      end
+    end
+    
   end
 end
