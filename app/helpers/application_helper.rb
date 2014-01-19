4bfe7d920570feb2c16af4eeab679e84a6c4e56e
Mohammed Mahdi Akkouh <akkouh.mahdi@gmail.com>
2014-01-19 12:27:15 +0100

    resizing caorusel images and adding english text

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
 	# Returns the full title on a per-page basis
 	def full_title(page_title)
-		base_title = "Sport In The City"
+		base_title = "Sport Everywhere"
 		if page_title.empty?
 			base_title
 		else
