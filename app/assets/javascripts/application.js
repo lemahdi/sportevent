169ec57166fedc183654d51c0654adbca0aedd0b
Mohammed Mahdi Akkouh <akkouh.mahdi@gmail.com>
2014-03-01 12:24:16 +0100

    adding some ie support and centeralizing image responsiveness

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,13 @@
 //= require leaflet
 
 //= require_tree .
+
+if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+  var msViewportStyle = document.createElement('style')
+  msViewportStyle.appendChild(
+    document.createTextNode(
+      '@-ms-viewport{width:auto!important}'
+    )
+  )
+  document.querySelector('head').appendChild(msViewportStyle)
+}
