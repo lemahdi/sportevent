d205372aad23cc488a7d13c3a1ca563c76ec999b
Mohammed Mahdi Akkouh <akkouh.mahdi@gmail.com>
2014-04-27 00:26:57 +0200

    adding uniqness check for name in the model Group

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,7 +4,8 @@ class Group < ActiveRecord::Base
   has_many :contracts, dependent: :destroy
   has_many :users, through: :contracts
 
-  validates :name, presence: true
+  validates :name, presence: true,
+									 uniqueness: true
 
   scope :asc, -> attr { order("groups.#{attr} ASC") }
 end
